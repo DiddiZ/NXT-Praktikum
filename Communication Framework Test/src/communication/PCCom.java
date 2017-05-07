f5f440d0fd4b5ad7d492d69fd9cc6a379ed2d650
Gregor Kobsik <Gregor.kobsik@gmail.com>
2017-05-07 22:29:07 +0200

    implemented callback interface for functions, removed meta files from version control

--- Communication Framework Test/src/communication/PCCom.java
+++ Communication Framework Test/src/communication/PCCom.java
@@ -1,76 +1,78 @@
-//author: Gregor
+/**
+ * This class provides an interface to connect the NXT brick with a PC through Bluetooth.
+ * It offers a functionality to register callback methods. Up to 32 methods can be registered.
+ * 
+ * @author Gregor
+ * 
+ */
 
 package communication;
 
 import java.io.*;
-import lejos.nxt.*;
 import lejos.nxt.comm.*;
+
+import communication.callbackMethods;
 
 public class PCCom extends Thread{
 	
-	public static DataOutputStream dataOut; 
-	public static DataInputStream dataIn;
-	public static USBConnection USBLink;
-	public static BTConnection BTLink;
-	public static BTConnection btLink;
-	public static int transmitReceived;
-	private boolean isConnected = false, isNewData = false;	
+	public DataOutputStream dataOut; 
+	public DataInputStream dataIn;
+	public BTConnection BTLink;
+	public int transmitReceived;
+	private boolean isConnected, isConnecting;	
 	
-	public PCCom() {
-		connect();
-		this.start();
+	//hold up to 32 callback methods
+	private callbackMethods[] callbackArray = new callbackMethods[32];
+	
+	/**
+	 * constructor of the PCCom(munication) class.
+	 */
+	public PCCom() {		
+		isConnected = isConnecting = false;
 	}
 	
-	
+	/**
+	 * This is the main function of the thread. It is polling for transmitted data, while the NXT is connected to a PC.
+	 */
 	public void run() {
-		while (isConnected)
+		while (isConnected) {
 			checkCommand();
-	}
-	
-	
-	private void checkCommand() {
-		try {
-		       transmitReceived = dataIn.read();
-		       System.out.print((char) transmitReceived);
-		       isNewData = true;
-
-		       }
-		    
-		    catch (IOException ioe) {
-		       System.out.println("IO Exception read");
-		       }
-		
-		if (transmitReceived == -1) {
-	    	   System.out.println("Connection lost...");
-	    	   isConnected = false;
+			Thread.yield();
 		}
 	}
 	
+	/**
+	 * @return returns if the NXT is trying to establish a connection
+	 */
+	public boolean getConnecting() {
+		return isConnecting;
+	}
+	
+	/**
+	 * @return returns if the NXT is connected to a PC
+	 */
 	public boolean getConnected() {
 		return isConnected;
 	}
 	
-	public int getNewData() {
-		if (isNewData) {
-			isNewData = false;
-			return transmitReceived;
-		} else {
-			return -1;
-		}
-	}
-	
+	/**
+	 * This function tries to establish a connection with the PC.
+	 */
 	public void connect() {
-	    System.out.println("Listening");
+		isConnecting = true;
+	    System.out.println("Awaiting connection.");
 	    BTLink = Bluetooth.waitForConnection();
 	    isConnected = true;
 	    dataOut = BTLink.openDataOutputStream();
 	    dataIn = BTLink.openDataInputStream();
-	    System.out.println("Waiting for input.");
-	   //USBLink = USB.waitForConnection();
-	   //dataOut = USBLink.openDataOutputStream();
-	   //dataIn = USBLink.openDataInputStream();
+	    System.out.println("Ready for input.");
+	    isConnecting = false;
+	    this.start();
 	}
 	
+	/**
+	 * This function disconnects any connection of the NXT.
+	 */
 	public void disconnect() {
 		if (BTLink != null)
 			BTLink.close();
@@ -84,6 +86,220 @@ public class PCCom extends Thread{
 		}
 		isConnected = false;
 		System.out.println("Disconnected");
+	}
+	
+	/**
+	 * This function tries to send data to an open connection.
+	 * @param p_data data that should be transmitted
+	 * @return returns if the data transfer was successful
+	 */
+	public boolean sendInteger(int p_data) {
+		if (isConnected && dataOut != null) {
+			try {
+				dataOut.writeInt(p_data);
+			} catch (IOException ioExc) {
+				System.out.println("Sending data failed.");
+				return false;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * This function registers a callback function to a position via the callbackMethods interface.
+	 * The position of the registered function must match the interface method. 
+	 * @param p_interface The callbackMethods interface of the class.
+	 * @param p_pos The position at which the callback method will be registered. 
+	 * @return Returns true if the position was unused and the callback method could be registered, otherwise false.
+	 */
+	public boolean registerCallback(callbackMethods p_interface, int p_pos) {
+		if (p_pos > 31 || p_pos < 0) {
+			return false;
+		}
+		if (callbackArray[p_pos] != null) {
+			return false;
+		}
+		callbackArray[p_pos] = p_interface;		
+		return true;
+	}
+	
+	/**
+	 * This function unregisters a function in the callBack array.
+	 * @param p_pos The position that should be freed.
+	 * @return Returns true if a method was unregistered, otherwise false.
+	 */
+	public boolean unregisterCallback(int p_pos) {
+		if (callbackArray[p_pos] != null) {
+			callbackArray[p_pos] = null;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * This function tries to read an incoming data and call the callback method according the the message.
+	 */
+	private void checkCommand() {
+		try {
+		       transmitReceived = dataIn.read();
+		       int callbackPosition = translateInputToCallbackPosition(transmitReceived);
+		       int callbackParameter = translateInputToCallbackParameter(transmitReceived);
+		       callBack(callbackPosition,callbackParameter);
+		       System.out.print((char) transmitReceived);
+
+		       }
+		    
+		    catch (IOException ioe) {
+		       System.out.println("IO Exception read");
+		       }
+		
+		if (transmitReceived == -1 || transmitReceived == -2) {
+	    	   System.out.println("Connection lost...");
+	    	   disconnect();
+		}
+	}
+	
+	/**
+	 * This function extracts the callback method position from an integer input
+	 * 0bXXXXXX__________________________ (6bit|26bit)
+	 */
+	private int translateInputToCallbackPosition(int p_input) {
+		p_input = p_input >> 26;
+		p_input &= 0b111111;
+		return p_input;
+	}
+	
+	/**
+	 * This function extracts the callback parameter from an integer input
+	 * 0b______XXXXXXXXXXXXXXXXXXXXXXXXXX (6bit|26bit)
+	 */
+	private int translateInputToCallbackParameter(int p_input) {
+		p_input &= 0b00000011111111111111111111111111;
+		return p_input;
+	}
+	
+	/**
+	 * This function calls a registered callBack method according to the input parameters.
+	 * @param p_pos The position of the registered callback method in the callback array.
+	 * @param p_parameter The parameters that will be used to call the function.
+	 */
+	private void callBack(int p_pos, int p_parameter) {
+		if (p_pos < 0 || p_pos > 31) {
+			System.out.println("Tried to callBack with invalid position.");
+			return;
+		}
+		if (p_parameter < 0 || p_parameter > (Math.pow(2, 26)-1) ) {
+			System.out.println("Tried to callBack with invalid parameter.");
+			return;
+		}
+		if (callbackArray[p_pos] != null) {
+			switch (p_pos) {
+			case 0:
+				callbackArray[p_pos].callback0(p_parameter);
+				break;
+			case 1:
+				callbackArray[p_pos].callback1(p_parameter);
+				break;
+			case 2:
+				callbackArray[p_pos].callback2(p_parameter);
+				break;
+			case 3:
+				callbackArray[p_pos].callback3(p_parameter);
+				break;
+			case 4:
+				callbackArray[p_pos].callback4(p_parameter);
+				break;
+			case 5:
+				callbackArray[p_pos].callback5(p_parameter);
+				break;
+			case 6:
+				callbackArray[p_pos].callback6(p_parameter);
+				break;
+			case 7:
+				callbackArray[p_pos].callback7(p_parameter);
+				break;
+			case 8:
+				callbackArray[p_pos].callback8(p_parameter);
+				break;
+			case 9:
+				callbackArray[p_pos].callback9(p_parameter);
+				break;
+			case 10:
+				callbackArray[p_pos].callback10(p_parameter);
+				break;
+			case 11:
+				callbackArray[p_pos].callback11(p_parameter);
+				break;
+			case 12:
+				callbackArray[p_pos].callback12(p_parameter);
+				break;
+			case 13:
+				callbackArray[p_pos].callback13(p_parameter);
+				break;
+			case 14:
+				callbackArray[p_pos].callback14(p_parameter);
+				break;
+			case 15:
+				callbackArray[p_pos].callback15(p_parameter);
+				break;
+			case 16:
+				callbackArray[p_pos].callback16(p_parameter);
+				break;
+			case 17:
+				callbackArray[p_pos].callback17(p_parameter);
+				break;
+			case 18:
+				callbackArray[p_pos].callback18(p_parameter);
+				break;
+			case 19:
+				callbackArray[p_pos].callback19(p_parameter);
+				break;
+			case 20:
+				callbackArray[p_pos].callback20(p_parameter);
+				break;
+			case 21:
+				callbackArray[p_pos].callback21(p_parameter);
+				break;
+			case 22:
+				callbackArray[p_pos].callback22(p_parameter);
+				break;
+			case 23:
+				callbackArray[p_pos].callback23(p_parameter);
+				break;
+			case 24:
+				callbackArray[p_pos].callback24(p_parameter);
+				break;
+			case 25:
+				callbackArray[p_pos].callback25(p_parameter);
+				break;
+			case 26:
+				callbackArray[p_pos].callback26(p_parameter);
+				break;
+			case 27:
+				callbackArray[p_pos].callback27(p_parameter);
+				break;
+			case 28:
+				callbackArray[p_pos].callback28(p_parameter);
+				break;
+			case 29:
+				callbackArray[p_pos].callback29(p_parameter);
+				break;
+			case 30:
+				callbackArray[p_pos].callback30(p_parameter);
+				break;
+			case 31:
+				callbackArray[p_pos].callback31(p_parameter);
+				break;				
+			} //end switch
+		} else {
+			System.out.println("No callback method registered at position:");
+			System.out.print(p_pos);
+			System.out.print(" parameter: ");
+			System.out.print(p_parameter);
+		} //end if
 	}
 	
 }
---
+++ Communication Framework Test/src/communication/callbackMethods.java
@@ -0,0 +1,84 @@
+/**
+ * This interface is used to register callback methods in the PCCom class.
+ * A function needs to implement this interface to be able to register methods in PCCom
+ * 
+ * @author Gregor
+ */
+
+package communication;
+
+public interface callbackMethods {
+
+	void callback0 (int p_parameter);
+	void callback1 (int p_parameter);
+	void callback2 (int p_parameter);
+	void callback3 (int p_parameter);
+	void callback4 (int p_parameter);
+	void callback5 (int p_parameter);
+	void callback6 (int p_parameter);
+	void callback7 (int p_parameter);
+	void callback8 (int p_parameter);
+	void callback9 (int p_parameter);
+	void callback10 (int p_parameter);
+	void callback11 (int p_parameter);
+	void callback12 (int p_parameter);
+	void callback13 (int p_parameter);
+	void callback14 (int p_parameter);
+	void callback15 (int p_parameter);
+	void callback16 (int p_parameter);
+	void callback17 (int p_parameter);
+	void callback18 (int p_parameter);
+	void callback19 (int p_parameter);
+	void callback20 (int p_parameter);
+	void callback21 (int p_parameter);
+	void callback22 (int p_parameter);
+	void callback23 (int p_parameter);
+	void callback24 (int p_parameter);
+	void callback25 (int p_parameter);
+	void callback26 (int p_parameter);
+	void callback27 (int p_parameter);
+	void callback28 (int p_parameter);
+	void callback29 (int p_parameter);
+	void callback30 (int p_parameter);
+	void callback31 (int p_parameter);
+	
+	/* copypaste this section to your class if you want to implement this interface.
+	 
+	public void callback0 (int p_parameter) {}
+ 	public void callback1 (int p_parameter) {}
+ 	public void callback2 (int p_parameter) {}
+ 	public void callback3 (int p_parameter) {}
+ 	public void callback4 (int p_parameter) {}
+ 	public void callback5 (int p_parameter) {}
+ 	public void callback6 (int p_parameter) {}
+ 	public void callback7 (int p_parameter) {}
+ 	public void callback8 (int p_parameter) {}
+ 	public void callback9 (int p_parameter) {}
+ 	public void callback10 (int p_parameter) {}
+ 	public void callback11 (int p_parameter) {}
+ 	public void callback12 (int p_parameter) {}
+ 	public void callback13 (int p_parameter) {}
+ 	public void callback14 (int p_parameter) {}
+ 	public void callback15 (int p_parameter) {}
+ 	public void callback16 (int p_parameter) {}
+ 	public void callback17 (int p_parameter) {}
+ 	public void callback18 (int p_parameter) {}
+ 	public void callback19 (int p_parameter) {}
+ 	public void callback20 (int p_parameter) {}
+ 	public void callback21 (int p_parameter) {}
+ 	public void callback22 (int p_parameter) {}
+ 	public void callback23 (int p_parameter) {}
+ 	public void callback24 (int p_parameter) {}
+ 	public void callback25 (int p_parameter) {}
+ 	public void callback26 (int p_parameter) {}
+ 	public void callback27 (int p_parameter) {}
+ 	public void callback28 (int p_parameter) {}
+ 	public void callback29 (int p_parameter) {}
+ 	public void callback30 (int p_parameter) {}
+	public void callback31 (int p_parameter) {}
+	  
+	 
+	 */
+	
+	
+}
