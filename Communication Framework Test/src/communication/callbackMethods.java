fd6e5516682261498e635822e72cb974e4b77404
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2017-05-10 12:25:24 +0200

    modified communication protokol

--- Communication Framework Test/src/communication/callbackMethods.java
+++ Communication Framework Test/src/communication/callbackMethods.java
@@ -7,75 +7,49 @@
 
 package communication;
 
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+
 public interface callbackMethods {
 
-	void callback0 (int p_parameter);
-	void callback1 (int p_parameter);
-	void callback2 (int p_parameter);
-	void callback3 (int p_parameter);
-	void callback4 (int p_parameter);
-	void callback5 (int p_parameter);
-	void callback6 (int p_parameter);
-	void callback7 (int p_parameter);
-	void callback8 (int p_parameter);
-	void callback9 (int p_parameter);
-	void callback10 (int p_parameter);
-	void callback11 (int p_parameter);
-	void callback12 (int p_parameter);
-	void callback13 (int p_parameter);
-	void callback14 (int p_parameter);
-	void callback15 (int p_parameter);
-	void callback16 (int p_parameter);
-	void callback17 (int p_parameter);
-	void callback18 (int p_parameter);
-	void callback19 (int p_parameter);
-	void callback20 (int p_parameter);
-	void callback21 (int p_parameter);
-	void callback22 (int p_parameter);
-	void callback23 (int p_parameter);
-	void callback24 (int p_parameter);
-	void callback25 (int p_parameter);
-	void callback26 (int p_parameter);
-	void callback27 (int p_parameter);
-	void callback28 (int p_parameter);
-	void callback29 (int p_parameter);
-	void callback30 (int p_parameter);
-	void callback31 (int p_parameter);
+	void callback0 (float p_parameter);
+	void callback1 (float p_parameter);
+	
 	
 	/* copypaste this section to your class if you want to implement this interface.
 	 
-	public void callback0 (int p_parameter) {}
- 	public void callback1 (int p_parameter) {}
- 	public void callback2 (int p_parameter) {}
- 	public void callback3 (int p_parameter) {}
- 	public void callback4 (int p_parameter) {}
- 	public void callback5 (int p_parameter) {}
- 	public void callback6 (int p_parameter) {}
- 	public void callback7 (int p_parameter) {}
- 	public void callback8 (int p_parameter) {}
- 	public void callback9 (int p_parameter) {}
- 	public void callback10 (int p_parameter) {}
- 	public void callback11 (int p_parameter) {}
- 	public void callback12 (int p_parameter) {}
- 	public void callback13 (int p_parameter) {}
- 	public void callback14 (int p_parameter) {}
- 	public void callback15 (int p_parameter) {}
- 	public void callback16 (int p_parameter) {}
- 	public void callback17 (int p_parameter) {}
- 	public void callback18 (int p_parameter) {}
- 	public void callback19 (int p_parameter) {}
- 	public void callback20 (int p_parameter) {}
- 	public void callback21 (int p_parameter) {}
- 	public void callback22 (int p_parameter) {}
- 	public void callback23 (int p_parameter) {}
- 	public void callback24 (int p_parameter) {}
- 	public void callback25 (int p_parameter) {}
- 	public void callback26 (int p_parameter) {}
- 	public void callback27 (int p_parameter) {}
- 	public void callback28 (int p_parameter) {}
- 	public void callback29 (int p_parameter) {}
- 	public void callback30 (int p_parameter) {}
-	public void callback31 (int p_parameter) {}
+	public void callback0 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback1 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback2 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback3 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback4 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback5 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback6 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback7 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback8 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback9 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback10 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback11 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback12 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback13 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback14 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback15 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback16 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback17 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback18 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback19 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback20 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback21 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback22 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback23 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback24 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback25 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback26 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback27 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback28 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback29 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback30 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
+	public void callback31 (DataOutputStream p_dataOut, DataInputStream p_dataIn);
 	  
 	 
 	 */
