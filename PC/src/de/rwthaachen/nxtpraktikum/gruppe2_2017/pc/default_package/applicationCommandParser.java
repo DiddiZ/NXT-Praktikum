20460aceac33f3679901048ec76a4bb67823f46b
Fabian Paul Ferdinand Friedrichs <fabian.friedrichs@rwth-aachen.de>
2017-05-29 12:52:17 +0200

    Added handler class for GUI; Added parser for command line in GUI

---
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/default_package/applicationCommandParser.java
@@ -0,0 +1,5 @@
+package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.default_package;
+
+public class applicationCommandParser {
+
+}
---
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/default_package/applicationHandler.java
@@ -0,0 +1,91 @@
+package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.default_package;
+
+public class applicationHandler {
+	//Connect Area
+	public void connectButton(){
+		
+	}
+	//Parameter Area
+	
+	//Command Area
+	public void sendCommandButton(){
+		
+	}
+	//PositionTab
+	public void goForwardButton(){
+		
+	}
+	
+	public void goBackButton(){
+		
+	}
+	
+	public void goLeftButton(){
+		
+	}
+	
+	public void goRightButton(){
+		
+	}
+	
+	public void driveDistanceButton(){
+		
+	}
+	
+	public void turnAbsoluteButton(){
+		
+	}
+	
+	public void turnRelativeButton(){
+		
+	}
+	
+	public void driveToButton(){
+		
+	}
+	
+	//ParameterTab
+	public void sendGyroSpeedButton(){
+		
+	}
+	
+	public void sendGyroIntegralButton(){
+		
+	}
+	
+	public void sendMototDistanceButton(){
+		
+	}
+	
+	public void sendConstantSpeedButton(){
+		
+	}
+	
+	public void sendConstantRotationButton(){
+		
+	}
+	
+	public void sendWheeldiameterButton(){
+		
+	}
+	
+	public void sendTrackButton(){
+		
+	}
+	//MapTab
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
