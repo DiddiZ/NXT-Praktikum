d2f002e3f94f664144124106e6ecb0021f9de61f
Justus <Justus@DESKTOP-MPI7PF3>
2017-06-12 17:03:48 +0200

    First attempt on LogInfoHandler

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/LogInfoHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/LogInfoHandler.java
@@ -1,5 +1,25 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn;
 
-public class LogInfoHandler {
+/**
+ * This class handles the LOG_INFO messages
+ * @author Justus
+ * 
+ */
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
+import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.*;
+
+public class LogInfoHandler implements CommandHandler{
+	@Override
+	public void handle(DataInputStream is) throws IOException {
+		final byte length = is.readByte();
+		byte[] message = new byte[length];
+		for (byte b = 0; b < length; b++){
+			message[b] = is.readByte();
+		}
+		// TODO convert the array to a string and print the LOG_INFO
+		// TODO catch errors
+	}
 }
