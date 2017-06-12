303daa00fbe9e21701f8e18526c9998dbc342c42
Justus <Justus@DESKTOP-MPI7PF3>
2017-06-12 17:44:22 +0200

    Several little changes, first attempt on ProtocolVersions

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/comm/CommandIdList.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/comm/CommandIdList.java
@@ -11,14 +11,15 @@ public final class CommandIdList {
 	 * This command id list belongs to the protocol number 0.
 	 */
 	public final static byte 
-	COMMAND_SET 		= 1,
-	COMMAND_GET 		= 2,
-	COMMAND_GET_RETURN 	= 3,
-	COMMAND_MOVE 		= 4,
-	COMMAND_TURN 		= 5,
-	COMMAND_MOVE_TO 	= 6,
-	COMMAND_BALANCING 	= 7,
-	COMMAND_LOG_INFO 	= 8,
-	COMMAND_ERROR_CODE 	= 9,
-	COMMAND_DISCONNECT 	= 10;
+	COMMAND_SET 				= 1,
+	COMMAND_GET 				= 2,
+	COMMAND_GET_RETURN 			= 3,
+	COMMAND_MOVE 				= 4,
+	COMMAND_TURN 				= 5,
+	COMMAND_MOVE_TO 			= 6,
+	COMMAND_BALANCING 			= 7,
+	COMMAND_LOG_INFO 			= 8,
+	COMMAND_ERROR_CODE 			= 9,
+	COMMAND_DISCONNECT 			= 10;
+	COMMAND_PROTOCOL_VERSION 	= 11;
 }
