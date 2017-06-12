823758535ff19c57bc184a8067b5b876e7157236
Fabian Paul Ferdinand Friedrichs <fabian.friedrichs@rwth-aachen.de>
2017-06-12 12:22:47 +0200

    Added methods to check if Strings are convertable to certain types

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/default_package/applicationCommandParser.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/default_package/applicationCommandParser.java
@@ -1,5 +1,41 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.default_package;
 
 public class applicationCommandParser {
+	
+	public static boolean byteConvertable(String arg){
+		try{
+			byte a=Byte.parseByte(arg);
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
+	public static boolean intConvertable(String arg){
+		try{
+			int a=Integer.parseInt(arg);
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
+	public static boolean floatConvertable(String arg){
+		try{
+			float a=Float.parseFloat(arg);
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
+	public static boolean longConvertable(String arg){
+		try{
+			long a=Long.parseLong(arg);
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
 
 }
