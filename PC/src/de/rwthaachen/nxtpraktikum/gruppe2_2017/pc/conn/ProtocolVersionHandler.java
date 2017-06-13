c1bb54a773a320f441e379ad042a88ad3fb92a15
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2017-06-13 19:13:52 +0200

    finished implementation of communication protocol, tests missing.

---
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/ProtocolVersionHandler.java
@@ -0,0 +1,30 @@
+package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn;
+
+/**
+ * This class handles the LOG_INFO messages
+ * @author Justus
+ * 
+ */
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
+import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.*;
+import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.CommunicatorPC;
+
+public class ProtocolVersionHandler implements CommandHandler{
+	@Override
+	public void handle(DataInputStream is) throws IOException {
+		final byte protocolVersion = is.readByte();
+		System.out.println("Connected with protocol version " + protocolVersion + ".");
+		if (protocolVersion != 2) {
+			System.out.println("GUI will use only standard commands.");
+		} else {
+			System.out.println("GUI will use extended command list.");
+		}
+		
+		CommunicatorPC.setProtocolVersion(protocolVersion);
+		
+		
+	}
+}
