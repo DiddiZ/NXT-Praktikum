19cc8f56eb8adbed21b0b7090c0bc332a364ceb9
Christian Kloos <Christian Kloos@134.61.184.202>
2017-06-15 17:28:44 +0200

    added wrapper for output

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/GetReturnHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/GetReturnHandler.java
@@ -22,17 +22,14 @@ public final class GetReturnHandler implements CommandHandler
 		switch (param) {
 			case BATTERY_VOLTAGE:
 				final int voltage = is.readInt();
-				System.out.println("Voltage: " + voltage + "V");
-				Display.getDefault().syncExec(new Runnable() {public void run() {
-					    application.output(""+voltage);
-				}});
-				//application.output(""+voltage);
+				//System.out.println("Voltage: " + voltage + "V");
+				SyncExec.syncsetBatteryLabel(voltage);
 				//application.setBatteryLabel(voltage);
 				break;
 			case GYRO_ANGLE:
 				final float angle = is.readFloat();
 				//System.out.println("Gyro angle: " + angle + "°");
-				application.setTiltLabel(angle);
+				SyncExec.syncsetTiltLabel(angle);
 				break;
 			case TACHO_LEFT:
 				final long tachoLeft = is.readLong();
@@ -45,55 +42,55 @@ public final class GetReturnHandler implements CommandHandler
 			case HEADING:
 				final float heading = is.readFloat();
 				//System.out.println("Heading: " + heading + "°");
-				application.setRotationLabel(heading);
+				SyncExec.syncsetRotationLabel(heading);
 				break;
 			case POSITION:
 				final float posX = is.readFloat();
 				final float posY = is.readFloat();
 				//System.out.println("Position: (" + posX + ", " + posY + ")");
-				application.setPositionLabel(posX, posY);
+				SyncExec.syncsetPositionLabel(posX, posY);
 				break;
 			case MOVEMENT_SPEED:
 				final float movementSpeed = is.readFloat();
 				//System.out.println("Movement speed: " + movementSpeed + "cm/s");
-				application.setSpeedometerLabel(movementSpeed);
+				SyncExec.syncsetSpeedometerLabel(movementSpeed);
 				break;
 			case STATUS_PACKAGE:
 				final float posX_all = is.readFloat();
 				final float posY_all = is.readFloat();
 				final float movementSpeed_all = is.readFloat();
 				final float heading_all = is.readFloat();
-				System.out.println("Position: (" + posX_all + ", " + posY_all + ")");
-				System.out.println("Movement speed: " + movementSpeed_all + "cm/s");
-				System.out.println("Heading: " + heading_all + "°");
-				//application.setPositionLabel(posX_all, posY_all);
-				//application.setSpeedometerLabel(movementSpeed_all);
-				//application.setRotationLabel(heading_all);
+				//System.out.println("Position: (" + posX_all + ", " + posY_all + ")");
+				//System.out.println("Movement speed: " + movementSpeed_all + "cm/s");
+				//System.out.println("Heading: " + heading_all + "°");
+				SyncExec.syncsetSpeedometerLabel(movementSpeed_all);
+				SyncExec.syncsetPositionLabel(posX_all, posY_all);
+				SyncExec.syncsetRotationLabel(heading_all);
 				break;
 			case AUTO_STATUS_PACKAGE:
 				final boolean isOn = is.readBoolean();
 				//System.out.println("Auto status package: " + isOn);
-				application.setAutoStatusPacket(isOn);
+				SyncExec.syncsetAutoStatusPacket(isOn);
 				break;
 			case PID_GYRO_SPEED:
 				final double gyroSpeed = is.readDouble();
 				//System.out.println("Gyro speed weight: " + gyroSpeed);
-				application.setGyroSpeedt(gyroSpeed);
+				SyncExec.syncsetGyroSpeed(gyroSpeed);
 				break;
 			case PID_GYRO_INTEGRAL:
 				final double gyroIntegral = is.readDouble();
 				//System.out.println("Gyro integral weight: " + gyroIntegral);
-				application.setGyroIntegralt(gyroIntegral);
+				SyncExec.syncsetGyroIntegral(gyroIntegral);
 				break;
 			case PID_MOTOR_DISTANCE:
 				final double motorDistance = is.readDouble();
 				//System.out.println("Motor distance weight: " + motorDistance);
-				application.setMotorDistancet(motorDistance);
+				SyncExec.syncsetMotorDistance(motorDistance);
 				break;
 			case PID_MOTOR_SPEED:
 				final double motorSpeed = is.readDouble();
 				//System.out.println("Motor speed weight: " + motorSpeed);
-				application.setMotorSpeedt(motorSpeed);
+				SyncExec.syncsetMotorSpeed(motorSpeed);
 				break;
 			case PID_WEIGHT_ALL:
 				final double gyroSpeed_all = is.readDouble();
@@ -104,10 +101,10 @@ public final class GetReturnHandler implements CommandHandler
 				//System.out.println("Gyro integral weight: " + gyroIntegral_all);
 				//System.out.println("Motor distance weight: " + motorDistance_all);
 				//System.out.println("Motor speed weight: " + motorSpeed_all);
-				application.setGyroSpeedt(gyroSpeed_all);
-				application.setGyroIntegralt(gyroIntegral_all);
-				application.setMotorDistancet(motorDistance_all);
-				application.setMotorSpeedt(motorSpeed_all);
+				SyncExec.syncsetGyroSpeed(gyroSpeed_all);
+				SyncExec.syncsetGyroIntegral(gyroIntegral_all);
+				SyncExec.syncsetMotorDistance(motorDistance_all);
+				SyncExec.syncsetMotorSpeed(motorSpeed_all);
 				break;
 			default:
 				System.out.println("Unrecognized GetReturn command with " + param);
