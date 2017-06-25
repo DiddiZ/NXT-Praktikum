6acf1ee6780e0545043b0625f251bb9b3bfc2f5c
DiddiZ <robbzkupper@web.de>
2017-06-25 15:15:32 +0200

    Some source formatting cleanup.

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/DisconnectHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/DisconnectHandler.java
@@ -1,20 +1,19 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm.handler;
-/**
- * @author Gregor & Justus
- * 
- * This class handles the COMMAND_DISCONNECT calls.
- * 
- */
+
 import java.io.DataInputStream;
 import java.io.IOException;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm.CommunicatorNXT;
 
-public class DisconnectHandler implements CommandHandler{
-
+/**
+ * This class handles the COMMAND_DISCONNECT calls.
+ *
+ * @author Gregor & Justus
+ */
+public class DisconnectHandler implements CommandHandler
+{
 	@Override
 	public void handle(DataInputStream is) throws IOException {
-		CommunicatorNXT.staticDisconnect();		
+		CommunicatorNXT.staticDisconnect();
 	}
-
 }
