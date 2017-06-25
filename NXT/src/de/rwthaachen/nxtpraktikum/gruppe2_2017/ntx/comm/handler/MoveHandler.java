8b51db632925eb198a031bb9fb5eadeeb2a4eed1
DiddiZ <robbzkupper@web.de>
2017-06-25 14:59:43 +0200

    implemented MoveHandler.

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/MoveHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/MoveHandler.java
@@ -1,21 +1,21 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm.handler;
+
 /**
- * @author Gregor & Justus
- * 
  * This class handles the COMMAND_MOVE calls.
- * 
+ *
+ * @author Gregor & Justus & Robin
  */
 import java.io.DataInputStream;
 import java.io.IOException;
-
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
+import de.rwthaachen.nxtpraktikum.gruppe2_2017.nxt.motorcontrol.MotorController;
 
-public class MoveHandler implements CommandHandler {
-
+public class MoveHandler implements CommandHandler
+{
 	@Override
 	public void handle(DataInputStream is) throws IOException {
-		System.out.println("moveHandler");
-		
+		final float distance = is.readFloat();
+		MotorController.move(distance);
+		System.out.println("Moving " + distance + "cm");
 	}
-
 }
