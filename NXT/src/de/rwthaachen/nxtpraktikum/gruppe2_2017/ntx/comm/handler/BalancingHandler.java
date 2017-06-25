bba802d37bff4f37dafbad0a6ecab4b36e872ca1
DiddiZ <robbzkupper@web.de>
2017-06-25 15:15:32 +0200

    Some source formatting cleanup.

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/BalancingHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/BalancingHandler.java
@@ -1,30 +1,25 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm.handler;
-/**
- * @author Gregor & Justus
- * 
- * This class handles the COMMAND_BALANCING calls.
- * 
- */
+
 import java.io.DataInputStream;
 import java.io.IOException;
-
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
-import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.Audio;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.SegwayMain;
-import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.sensors.SensorData;
-import de.rwthaachen.nxtpraktikum.gruppe2_2017.nxt.motorcontrol.MotorController;
 
-public class BalancingHandler implements CommandHandler{
+/**
+ * This class handles the COMMAND_BALANCING calls.
+ *
+ * @author Gregor & Justus & Robin
+ */
+public class BalancingHandler implements CommandHandler
+{
 
 	@Override
 	public void handle(DataInputStream is) throws IOException {
-		System.out.println("balancingHandler");
-		final boolean value = is.readBoolean();
-		if (value) {
+		final boolean enabled = is.readBoolean();
+		if (enabled)
 			SegwayMain.startBalancing();
-		} else {
+		else
 			SegwayMain.stopBalancing();
-		}
+		System.out.println("balancing=" + enabled);
 	}
-
 }
--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/TurnHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/TurnHandler.java
@@ -1,15 +1,15 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm.handler;
 
-/**
- * This class handles the COMMAND_TURN calls.
- *
- * @author Gregor & Justus & Robin
- */
 import java.io.DataInputStream;
 import java.io.IOException;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.nxt.motorcontrol.MotorController;
 
+/**
+ * This class handles the COMMAND_TURN calls.
+ *
+ * @author Gregor & Justus & Robin
+ */
 public class TurnHandler implements CommandHandler
 {
 	@Override
