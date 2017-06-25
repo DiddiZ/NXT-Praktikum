ed9aee0d9897cfc8b9e45d9d67c974c22ae702de
DiddiZ <robbzkupper@web.de>
2017-06-25 15:05:13 +0200

    Made AutoStatusThread send positions.

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/AutoStatusThread.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/AutoStatusThread.java
@@ -1,36 +1,35 @@
 /**
+ * AutoStatusThread sends the status package every 5000ms to the PC while activated.
+ *
  * @author Gregor
- * 
- * sends the status package every 5000ms to the PC while activated.
  */
 
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm;
 
 import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.STATUS_PACKET;
-
 import java.io.IOException;
-
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.NXT;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.sensors.SensorData;
 
-public class AutoStatusThread extends Thread {
-	
+public class AutoStatusThread extends Thread
+{
+
 	protected boolean isActivated = true;
-	
+
 	public AutoStatusThread() {
-		this.setDaemon(true);
-		this.setPriority(4);
+		setDaemon(true);
+		setPriority(4);
 	}
-	
+
 	public void activate() {
 		isActivated = true;
 		run();
 	}
-	
+
 	public void deactivate() {
 		isActivated = false;
 	}
-	
+
 	@Override
 	public void run() {
 		long nextTime = 0;
@@ -38,14 +37,14 @@ public class AutoStatusThread extends Thread {
 			// TODO get the position from corresponding class.
 			if (nextTime < System.currentTimeMillis()) {
 				try {
-					NXT.COMMUNICATOR.sendGetReturn(STATUS_PACKET, 
-							(float) 0, (float) 0, (float) SensorData.motorSpeed, (float) SensorData.heading);
-				} catch (IOException e) {
+					NXT.COMMUNICATOR.sendGetReturn(STATUS_PACKET,
+							(float)SensorData.positionX, (float)SensorData.positionY, (float)SensorData.motorSpeed, (float)SensorData.heading);
+				} catch (final IOException e) {
 					System.out.println("Could not sent AutoStatusPacket");
 				}
 				nextTime = System.currentTimeMillis() + 5000;
-			}	
-			//isActivated = false;
+			}
+			// isActivated = false;
 			yield();
 		}
 	}
