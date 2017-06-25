9e9de22f9a0357166dfb3a774fd5cc65fdc4dc8f
DiddiZ <robbzkupper@web.de>
2017-06-25 15:10:40 +0200

    Implemented TurnHandler logic.

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/TurnHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/TurnHandler.java
@@ -1,21 +1,21 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm.handler;
+
 /**
- * @author Gregor & Justus
- * 
  * This class handles the COMMAND_TURN calls.
- * 
+ *
+ * @author Gregor & Justus & Robin
  */
 import java.io.DataInputStream;
 import java.io.IOException;
-
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
+import de.rwthaachen.nxtpraktikum.gruppe2_2017.nxt.motorcontrol.MotorController;
 
-public class TurnHandler implements CommandHandler{
-
+public class TurnHandler implements CommandHandler
+{
 	@Override
 	public void handle(DataInputStream is) throws IOException {
-		System.out.println("turnHandler");
-		
+		final float angle = is.readFloat();
+		MotorController.turn(angle);
+		System.out.println("Turning by " + angle + "°");
 	}
-
 }
