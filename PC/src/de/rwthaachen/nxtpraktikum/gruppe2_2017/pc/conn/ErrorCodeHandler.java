0cd13eaf89a7b81b65375c95a3c5f84757ab5726
DiddiZ <robbzkupper@web.de>
2017-06-26 15:06:36 +0200

    Formatting.

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/ErrorCodeHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/ErrorCodeHandler.java
@@ -1,22 +1,23 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn;
-/**
- * This class handles the ERROR_CODE messages
- * @author Gregor & Justus
- *
- */
 
+import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.ERROR_CODE_FALLEN;
+import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.ERROR_CODE_PACKAGE_LOSS;
 import java.io.DataInputStream;
 import java.io.IOException;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.*;
 
-public class ErrorCodeHandler implements CommandHandler {
-
+/**
+ * This class handles the ERROR_CODE messages
+ *
+ * @author Gregor & Justus
+ */
+public class ErrorCodeHandler implements CommandHandler
+{
 	@Override
 	public void handle(DataInputStream is) throws IOException {
-		//System.out.println("Error code handler...");
+		// System.out.println("Error code handler...");
 		final byte param = is.readByte();
-		switch(param){
+		switch (param) {
 			case ERROR_CODE_FALLEN:
 				System.out.println("NXT has fallen.");
 				// TODO handle the error (disconnect etc)
@@ -27,8 +28,6 @@ public class ErrorCodeHandler implements CommandHandler {
 				break;
 			default:
 				System.out.println("Unrecognized Errorcode with " + param);
-				
 		}
 	}
-
 }
--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/ProtocolVersionHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/ProtocolVersionHandler.java
@@ -1,32 +1,28 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn;
 
-/**
- * This class handles the LOG_INFO messages
- * @author Justus
- * 
- */
-
 import java.io.DataInputStream;
 import java.io.IOException;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.*;
-import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.CommunicatorPC;
-import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui.application;
 
-public class ProtocolVersionHandler implements CommandHandler{
+/**
+ * This class handles the LOG_INFO messages
+ *
+ * @author Justus
+ */
+public class ProtocolVersionHandler implements CommandHandler
+{
 	@Override
 	public void handle(DataInputStream is) throws IOException {
-		final byte protocolVersion = is.readByte();
-		//application.output("Connected with protocol version " + protocolVersion + ".");
+		byte protocolVersion = is.readByte();
+		// application.output("Connected with protocol version " + protocolVersion + ".");
 		System.out.println("Connected with protocol version " + protocolVersion + ".");
 		if (protocolVersion != 2) {
 			System.out.println("GUI will use only standard commands.");
+			protocolVersion = 0;
 		} else {
 			System.out.println("GUI will use extended command list.");
 		}
-		
+
 		CommunicatorPC.setProtocolVersion(protocolVersion);
-		
-		
 	}
 }
