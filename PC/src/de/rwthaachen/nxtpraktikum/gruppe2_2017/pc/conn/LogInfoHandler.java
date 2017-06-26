99afb7b4327fc3b6aaa74635c3e9b9e14f03d12a
DiddiZ <robbzkupper@web.de>
2017-06-26 15:06:36 +0200

    Formatting.

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/LogInfoHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/LogInfoHandler.java
@@ -1,24 +1,24 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn;
-/**
- * This class handles the ERROR_CODE messages
- * @author Gregor & Justus
- *
- */
 
 import java.io.DataInputStream;
 import java.io.IOException;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.*;
 
-public class LogInfoHandler implements CommandHandler{
+/**
+ * This class handles the ERROR_CODE messages
+ *
+ * @author Gregor & Justus
+ */
+public class LogInfoHandler implements CommandHandler
+{
 	@Override
 	public void handle(DataInputStream is) throws IOException {
 		final byte length = is.readByte();
-		byte[] message = new byte[length];
-		for (byte b = 0; b < length; b++){
+		final byte[] message = new byte[length];
+		for (byte b = 0; b < length; b++) {
 			message[b] = is.readByte();
 		}
-		
+
 		// TODO convert the array to a string and print the LOG_INFO
 		// TODO catch errors
 		System.out.println(message);
