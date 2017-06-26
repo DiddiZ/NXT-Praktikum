e37d92cbe30145c51df85ff7749ff4b06ca84d86
DiddiZ <robbzkupper@web.de>
2017-06-26 15:06:36 +0200

    Formatting.

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/GetReturnHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/GetReturnHandler.java
@@ -1,35 +1,17 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn;
 
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.AUTO_STATUS_PACKET;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.BATTERY_VOLTAGE;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.GYRO_ANGLE;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.HEADING;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.MOVEMENT_SPEED;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PARAM_CONSTANT_ROTATION;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PARAM_CONSTANT_SPEED;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PARAM_TRACK;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PARAM_WHEEL_DIAMETER;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PID_GYRO_INTEGRAL;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PID_GYRO_SPEED;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PID_MOTOR_DISTANCE;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PID_MOTOR_SPEED;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.PID_WEIGHT_ALL;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.POSITION;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.STATUS_PACKET;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.TACHO_LEFT;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.TACHO_RIGHT;
-
+import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.*;
 /**
- * @author Gregor & Justus
- * 
  * This class handles the GET_RETURN command.
+ *
+ * @author Gregor & Justus
  */
 import java.io.DataInputStream;
 import java.io.IOException;
-
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 
-public final class GetReturnHandler implements CommandHandler {
+public final class GetReturnHandler implements CommandHandler
+{
 	@Override
 	public void handle(DataInputStream is) throws IOException {
 		final byte param = is.readByte();
@@ -40,7 +22,7 @@ public final class GetReturnHandler implements CommandHandler {
 				break;
 			case GYRO_ANGLE:
 				final float angle = is.readFloat();
-				//System.out.println("Gyro angle: " + angle + "°");
+				// System.out.println("Gyro angle: " + angle + "ï¿½");
 				SyncExec.syncsetTiltLabel(angle);
 				break;
 			case TACHO_LEFT:
@@ -53,18 +35,18 @@ public final class GetReturnHandler implements CommandHandler {
 				break;
 			case HEADING:
 				final float heading = is.readFloat();
-				//System.out.println("Heading: " + heading + "°");
+				// System.out.println("Heading: " + heading + "ï¿½");
 				SyncExec.syncsetRotationLabel(heading);
 				break;
 			case POSITION:
 				final float posX = is.readFloat();
 				final float posY = is.readFloat();
-				//System.out.println("Position: (" + posX + ", " + posY + ")");
+				// System.out.println("Position: (" + posX + ", " + posY + ")");
 				SyncExec.syncsetPositionLabel(posX, posY);
 				break;
 			case MOVEMENT_SPEED:
 				final float movementSpeed = is.readFloat();
-				//System.out.println("Movement speed: " + movementSpeed + "cm/s");
+				// System.out.println("Movement speed: " + movementSpeed + "cm/s");
 				SyncExec.syncsetSpeedometerLabel(movementSpeed);
 				break;
 			case STATUS_PACKET:
@@ -72,36 +54,36 @@ public final class GetReturnHandler implements CommandHandler {
 				final float posY_all = is.readFloat();
 				final float movementSpeed_all = is.readFloat();
 				final float heading_all = is.readFloat();
-				//System.out.println("Position: (" + posX_all + ", " + posY_all + ")");
-				//System.out.println("Movement speed: " + movementSpeed_all + "cm/s");
-				//System.out.println("Heading: " + heading_all + "°");
+				// System.out.println("Position: (" + posX_all + ", " + posY_all + ")");
+				// System.out.println("Movement speed: " + movementSpeed_all + "cm/s");
+				// System.out.println("Heading: " + heading_all + "ï¿½");
 				SyncExec.syncsetSpeedometerLabel(movementSpeed_all);
 				SyncExec.syncsetPositionLabel(posX_all, posY_all);
 				SyncExec.syncsetRotationLabel(heading_all);
 				break;
 			case AUTO_STATUS_PACKET:
 				final boolean isOn = is.readBoolean();
-				//System.out.println("Auto status package: " + isOn);
+				// System.out.println("Auto status package: " + isOn);
 				SyncExec.syncsetAutoStatusPacket(isOn);
 				break;
 			case PID_GYRO_SPEED:
 				final double gyroSpeed = is.readDouble();
-				//System.out.println("Gyro speed weight: " + gyroSpeed);
+				// System.out.println("Gyro speed weight: " + gyroSpeed);
 				SyncExec.syncsetGyroSpeed(gyroSpeed);
 				break;
 			case PID_GYRO_INTEGRAL:
 				final double gyroIntegral = is.readDouble();
-				//System.out.println("Gyro integral weight: " + gyroIntegral);
+				// System.out.println("Gyro integral weight: " + gyroIntegral);
 				SyncExec.syncsetGyroIntegral(gyroIntegral);
 				break;
 			case PID_MOTOR_DISTANCE:
 				final double motorDistance = is.readDouble();
-				//System.out.println("Motor distance weight: " + motorDistance);
+				// System.out.println("Motor distance weight: " + motorDistance);
 				SyncExec.syncsetMotorDistance(motorDistance);
 				break;
 			case PID_MOTOR_SPEED:
 				final double motorSpeed = is.readDouble();
-				//System.out.println("Motor speed weight: " + motorSpeed);
+				// System.out.println("Motor speed weight: " + motorSpeed);
 				SyncExec.syncsetMotorSpeed(motorSpeed);
 				break;
 			case PARAM_CONSTANT_ROTATION:
@@ -129,10 +111,10 @@ public final class GetReturnHandler implements CommandHandler {
 				final double gyroIntegral_all = is.readDouble();
 				final double motorDistance_all = is.readDouble();
 				final double motorSpeed_all = is.readDouble();
-				//System.out.println("Gyro speed weight: " + gyroSpeed_all);
-				//System.out.println("Gyro integral weight: " + gyroIntegral_all);
-				//System.out.println("Motor distance weight: " + motorDistance_all);
-				//System.out.println("Motor speed weight: " + motorSpeed_all);
+				// System.out.println("Gyro speed weight: " + gyroSpeed_all);
+				// System.out.println("Gyro integral weight: " + gyroIntegral_all);
+				// System.out.println("Motor distance weight: " + motorDistance_all);
+				// System.out.println("Motor speed weight: " + motorSpeed_all);
 				SyncExec.syncsetGyroSpeed(gyroSpeed_all);
 				SyncExec.syncsetGyroIntegral(gyroIntegral_all);
 				SyncExec.syncsetMotorDistance(motorDistance_all);
--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/SendGetThread.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/SendGetThread.java
@@ -1,87 +1,86 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui;
 
 import static de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.ParameterIdList.*;
-
-import org.eclipse.swt.widgets.Display;
-
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.SyncExec;
 
-public class SendGetThread extends application implements Runnable{	
-	public void run(){
+public class SendGetThread extends application implements Runnable
+{
+	@Override
+	public void run() {
 		int i = 0;
-		while(Send.com.isConnected()){
+		while (Send.com.isConnected()) {
 			switch (i) {
-			case 0:
-				SyncExec.syncoutput("Automatic update of values started.");
-				Send.sendGetByteQuiet(PID_GYRO_INTEGRAL);
-				Send.sendGetByteQuiet(PID_GYRO_SPEED);
-				Send.sendGetByteQuiet(PID_MOTOR_DISTANCE);
-				Send.sendGetByteQuiet(PID_MOTOR_SPEED);
-				Send.sendGetByteQuiet(BATTERY_VOLTAGE);
-				Send.sendGetByteQuiet(GYRO_ANGLE);
-				Send.sendGetByteQuiet(TACHO_LEFT);
-				Send.sendGetByteQuiet(TACHO_RIGHT);
-				Send.sendGetByteQuiet(HEADING);
-				Send.sendGetByteQuiet(POSITION);
-				Send.sendGetByteQuiet(MOVEMENT_SPEED);
-				Send.sendGetByteQuiet(PID_GYRO_INTEGRAL);
-				Send.sendGetByteQuiet(PARAM_CONSTANT_ROTATION);
-				Send.sendGetByteQuiet(PARAM_CONSTANT_SPEED);
-				Send.sendGetByteQuiet(PARAM_WHEEL_DIAMETER);
-				Send.sendGetByteQuiet(PARAM_TRACK);
-				break;
-			case 1:
-				Send.sendGetByteQuiet(PID_GYRO_SPEED);
-				break;
-			case 2:
-				Send.sendGetByteQuiet(PID_MOTOR_DISTANCE);
-				break;
-			case 3:
-				Send.sendGetByteQuiet(PID_MOTOR_SPEED);
-				break;
-			case 4:
-				Send.sendGetByteQuiet(BATTERY_VOLTAGE);
-				break;
-			case 5:
-				Send.sendGetByteQuiet(GYRO_ANGLE);
-				break;
-			case 6:
-				Send.sendGetByteQuiet(TACHO_LEFT);
-				break;
-			case 7:
-				Send.sendGetByteQuiet(TACHO_RIGHT);
-				break;
-			case 8:
-				Send.sendGetByteQuiet(HEADING);
-				break;
-			case 9:
-				Send.sendGetByteQuiet(POSITION);
-				break;
-			case 10:
-				Send.sendGetByteQuiet(MOVEMENT_SPEED);
-				break;
-			case 11:
-				Send.sendGetByteQuiet(PID_GYRO_INTEGRAL);
-				break;
-			case 12:
-				Send.sendGetByteQuiet(PARAM_CONSTANT_ROTATION);
-				break;
-			case 13:
-				Send.sendGetByteQuiet(PARAM_CONSTANT_SPEED);
-				break;
-			case 14:
-				Send.sendGetByteQuiet(PARAM_WHEEL_DIAMETER);
-				break;
-			case 15:
-				Send.sendGetByteQuiet(PARAM_TRACK);
-				break;
-			default:
-				i = 0;
-			}			
+				case 0:
+					SyncExec.syncoutput("Automatic update of values started.");
+					Send.sendGetByteQuiet(PID_GYRO_INTEGRAL);
+					Send.sendGetByteQuiet(PID_GYRO_SPEED);
+					Send.sendGetByteQuiet(PID_MOTOR_DISTANCE);
+					Send.sendGetByteQuiet(PID_MOTOR_SPEED);
+					Send.sendGetByteQuiet(BATTERY_VOLTAGE);
+					Send.sendGetByteQuiet(GYRO_ANGLE);
+					Send.sendGetByteQuiet(TACHO_LEFT);
+					Send.sendGetByteQuiet(TACHO_RIGHT);
+					Send.sendGetByteQuiet(HEADING);
+					Send.sendGetByteQuiet(POSITION);
+					Send.sendGetByteQuiet(MOVEMENT_SPEED);
+					Send.sendGetByteQuiet(PID_GYRO_INTEGRAL);
+					Send.sendGetByteQuiet(PARAM_CONSTANT_ROTATION);
+					Send.sendGetByteQuiet(PARAM_CONSTANT_SPEED);
+					Send.sendGetByteQuiet(PARAM_WHEEL_DIAMETER);
+					Send.sendGetByteQuiet(PARAM_TRACK);
+					break;
+				case 1:
+					Send.sendGetByteQuiet(PID_GYRO_SPEED);
+					break;
+				case 2:
+					Send.sendGetByteQuiet(PID_MOTOR_DISTANCE);
+					break;
+				case 3:
+					Send.sendGetByteQuiet(PID_MOTOR_SPEED);
+					break;
+				case 4:
+					Send.sendGetByteQuiet(BATTERY_VOLTAGE);
+					break;
+				case 5:
+					Send.sendGetByteQuiet(GYRO_ANGLE);
+					break;
+				case 6:
+					Send.sendGetByteQuiet(TACHO_LEFT);
+					break;
+				case 7:
+					Send.sendGetByteQuiet(TACHO_RIGHT);
+					break;
+				case 8:
+					Send.sendGetByteQuiet(HEADING);
+					break;
+				case 9:
+					Send.sendGetByteQuiet(POSITION);
+					break;
+				case 10:
+					Send.sendGetByteQuiet(MOVEMENT_SPEED);
+					break;
+				case 11:
+					Send.sendGetByteQuiet(PID_GYRO_INTEGRAL);
+					break;
+				case 12:
+					Send.sendGetByteQuiet(PARAM_CONSTANT_ROTATION);
+					break;
+				case 13:
+					Send.sendGetByteQuiet(PARAM_CONSTANT_SPEED);
+					break;
+				case 14:
+					Send.sendGetByteQuiet(PARAM_WHEEL_DIAMETER);
+					break;
+				case 15:
+					Send.sendGetByteQuiet(PARAM_TRACK);
+					break;
+				default:
+					i = 0;
+			}
 			i++;
 			try {
 				Thread.sleep(100);
-			} catch (InterruptedException e) {
+			} catch (final InterruptedException e) {
 				System.out.println("SendGetThread was interrupted while sleeping.");
 			}
 		}
