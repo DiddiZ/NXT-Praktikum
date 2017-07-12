8b38c514c89e9749e6aff799132a27dcdcd2f197
Fabian Paul Ferdinand Friedrichs <fabian.friedrichs@rwth-aachen.de>
2017-07-12 12:30:52 +0200

    Added Navigator Class.

---
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/Navigator.java
@@ -0,0 +1,24 @@
+package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui;
+
+import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.NXTData;
+
+public class Navigator extends Thread{
+	
+	private NXTData data;
+	private ApplicationHandler appHandler;
+	
+	private static final int MAP_SQUARE_LENGTH = 5;
+	
+	public Navigator(NXTData data, ApplicationHandler appHandler){
+		this.data = data;
+		this.appHandler = appHandler;
+	}
+	
+	public int  discrete(double param){
+		return (int) param / MAP_SQUARE_LENGTH * MAP_SQUARE_LENGTH;
+	}
+	
+	
+	
+
+}
