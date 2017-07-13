d41a840c5f1d80fed45475c605b6a098233df04e
DiddiZ <robbzkupper@web.de>
2017-07-13 23:12:26 +0200

    Use cones for US sensor. Rewrote MapData.

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/MapData.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/MapData.java
@@ -1,201 +1,43 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn;
 
+import java.awt.Point;
+import java.util.HashMap;
+
 /**
- * 
- * @author Justus
- *
- * This is a dynamic data structure to save all information 
- * gained by the NXT about his environment.
- * The Data is ordered by the x-coordinates in first place 
- * and in second place by the y-coordinates.
+ * @author Justus, Robin
+ *         This is a dynamic data structure to save all information
+ *         gained by the NXT about his environment.
+ *         The Data is ordered by the x-coordinates in first place
+ *         and in second place by the y-coordinates.
  */
 
+public class MapData extends HashMap<Point, Boolean>
+{
+	public void append(int x, int y, boolean isObstacle) {
+		put(new Point(x, y), isObstacle);
+	}
 
-public class MapData {
-	
-	private int x, y;
-	private boolean isObstacle;
-	public MapData next, previous;
-	
-	
-	/**
-	 * The constructor for a MapData object: 
-	 * @param x_init: the x-coordinate of the MapData
-	 * @param y_init: the y-coordinate of the MapData
-	 * @param obstacle_init: set to true, if the MapData marks an obstacle; otherwise, it will be a free area
-	 */
-	public MapData(int x_init, int y_init, boolean obstacle_init){
-		this.x = x_init;
-		this.y = y_init;
-		this.isObstacle = obstacle_init;
-		this.next = null;
-		this.previous = null;
-	}
-	
-	
-	/**
-	 * This method will add a new MapData to the existing Data (sorted)
-	 * @param newData: the new MapData that has to be added
-	 */
-	public void append(MapData newData){
-		/*The newData has already an existing entry in the MapData
-		 * if either the old or the new entry marks an obstacle, the Data will now mark this entry with obstacle
-		 */
-	
-		
-		if(this.x==newData.getX()&&this.y==newData.getY()){	
-			if(newData.getIsObstacle()){
-				this.isObstacle = true;
-			}
-		}
-		
-		//The newData is located before the actual entry in the MapData
-		if(this.x>newData.getX()||(this.x==newData.getX()&&this.y>newData.getY())){
-			//The actual entry is the first; newData becomes first instead
-			if(this.previous == null){
-				this.previous = newData;
-				newData.next = this;
-			}
-			else {
-			//NewData fits between the actual entry and the entry before
-				if(this.previous.getX()<newData.getX()||(this.previous.getX()==newData.getX()&&this.previous.getY()<newData.getY())){
-			
-					this.previous.next = newData;
-					newData.previous = this.previous;
-					this.previous = newData;
-					newData.next = this;
-				}
-			//NewData has a lower place in the order of the data
-				else{
-					this.previous.append(newData);
-				}
-			}	
-		}
-		
-		//The newData is located behind the actual entry in the MapData
-		if(this.x<newData.getX()||(this.x==newData.getX()&&this.y<newData.getY())){
-			//The actual entry is the last; newData becomes last instead
-			if(this.next==null){
-				this.next = newData;
-				newData.previous = this;
-			}
-			else{
-			//NewData fits between the actual entry and the entry behind
-				if(this.next.getX()>newData.getX()||(this.next.getX()==newData.getX()&&this.next.getY()>newData.getY())){
-					
-					this.next.previous = newData;
-					newData.next = this.next;
-					this.next = newData;
-					newData.previous = this;
-				}
-			//NewData has a higher place in the order of the MapData	
-				else{
-					this.next.append(newData);
-				}
-			}
-		}
-	}
-	
-	
 	/**
 	 * This method searches in the Data whether the coordinates are marked as an obstacle or free area
-	 * @param x_test: The x-coordinate of the Data the method searches for
-	 * @param y_test: The y-coordinate of the Data the method searches for
+	 *
+	 * @param x: The x-coordinate of the Data the method searches for
+	 * @param y: The y-coordinate of the Data the method searches for
 	 * @return: true, if a MapData exists that marks the Coordinates as an obstacle
-	 * 
-	 * 
 	 */
-	public boolean isObstacle(long x_test, long y_test){
-		MapData temp = this.getMapData(x_test, y_test);
-		if(temp != null){
-			return temp.getIsObstacle();
-		}
-		else{
-			return false;
-		}
+	public boolean isObstacle(int x, int y) {
+		final Boolean isObstacle = get(new Point(x, y));
+		return isObstacle == null || !isObstacle;
 	}
-	
-	
+
 	/**
 	 * This method searches for an entry in the Data with fitting coordinates
+	 *
 	 * @param x_test: The x-coordinate of the Data the method searches for
 	 * @param y_test: The y-coordinate of the Data the method searches for
 	 * @return: true, if an entry with fitting coordinates exists in the MapData
 	 */
-	public boolean isKnown(long x_test, long y_test){
-		if(this.getMapData(x_test, y_test)!=null){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	
-	/**
-	 * This method returns the x-coordinate of the MapData
-	 * @return the x-coordinate of the MapData
-	 */
-	public int getX(){
-		return this.x;
-	}
-	
-	
-	/**
-	 * This method returns the y-coordinate of the MapData
-	 * @return the y-coordinate of the MapData
-	 */
-	public int getY(){
-		return this.y;
-	}
-	
-	
-	/**
-	 * This method returns whether the Data is marked as an obstacle
-	 * @return true, if this Data marks an obstacle, otherwise returns false
-	 */
-	public boolean getIsObstacle(){
-		return this.isObstacle;
-	}
-	
-	
-	/**
-	 * This method returns a MapData to fitting coordinates
-	 * @param x_c: the x-coordinate of the MapData searched for
-	 * @param y_c: the y-coordinate of the MapData searched for
-	 * @return a MapData with fitting coordinates; null if there is no fitting MapData
-	 */
-	public MapData getMapData(long x_c, long y_c){
-		//return true, if the actual entry has the coordinates searched for
-				if(this.x==x_c && this.y==y_c){
-					return this;
-				}
-				
-				//if the coordinates are located before the actual entry
-				if(this.x>x_c||(this.x==x_c&&this.y>y_c)){
-					//if the previous entry has lower coordinates (or does not even exist), no fitting entry exists
-					if(this.previous == null || this.previous.getX()<x_c|| (this.previous.getX()==x_c&&this.previous.getY()<y_c)){
-						return null;
-					}
-					//otherwise search more in front of the data
-					else{
-						return this.previous.getMapData(x_c, y_c);
-					}
-				}
-				
-				//if the coordinates are located behind the actual entry
-				if(this.x<x_c||(this.x==x_c&&this.y<y_c)){
-					//if the next entry has higher coordinates (or does not even exist), no fitting entry exists
-					if(this.next == null || this.next.getX()>x_c || (this.next.getX()==x_c&&this.next.getY()>y_c)){
-						return null;
-					}
-					//otherwise search more in front of the data
-					else{
-						return this.next.getMapData(x_c, y_c);
-					}
-				}
-				
-				//THIS SHOULD NEVER BE THE CASE
-				return null;
+	public boolean isKnown(int x, int y) {
+		final Boolean isObstacle = get(new Point(x, y));
+		return isObstacle == null;
 	}
 }
--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/DrawingPanel.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/DrawingPanel.java
@@ -1,254 +1,197 @@
 package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui;
-import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.MapData;
+
 import static de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui.Navigator.MAP_SQUARE_LENGTH;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui.Navigator.OBSTACLE_DETECTION_RANGE;
-import static de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui.Navigator.OBSTACLE_DETECTION_WIDTH;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Map.Entry;
 import javax.swing.JPanel;
+import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.MapData;
+
 /**
  * @author Christian
  */
 class DrawingPanel extends JPanel
 {
-	private int width = 550;
-    private int height = 270;
-    private int posX = 0;
-    private int posY = 0;
-    private float head = 0;
-    final int ticNumber = 10; //axes marking number; higher equals more
-    final int ticSize = 150; //axes marking size; higher equals smaller
-    final int pointSize = 6; //point diameter in pixel
-    final int barrierLength = 20;
-    private List<Integer[]> obstacles = new ArrayList<Integer[]>();
-    private List<Integer[]> obstaclesPoints = new ArrayList<Integer[]>();
-    private Navigator navi;
-    private MapData map;
-    
-    public DrawingPanel(Navigator navi){
-    	this.navi = navi;
-    	map = navi.getMapData();
-    }
-    
-    public void setXY(int x, int y){
-    	posX = x;
-    	posY = y;
-    }
-    
-    public void setHeading(float heading) {
-		head = heading * (-1);
+	private final int width = 550;
+	private final int height = 270;
+	private int posX = 0;
+	private int posY = 0;
+	private float head = 0;
+	final int ticNumber = 10; // axes marking number; higher equals more
+	final int ticSize = 150; // axes marking size; higher equals smaller
+	final int pointSize = 6; // point diameter in pixel
+	final int barrierLength = 20;
+	private final List<Integer[]> obstacles = new ArrayList<>();
+	private final List<Integer[]> obstaclesPoints = new ArrayList<>();
+	private final MapData map;
+
+	public DrawingPanel(MapData map) {
+		this.map = map;
 	}
 
-    public void addNewNode(MapData data)
-    {
-    	map.append(data);
-    }
-    
-    public void newObstacle(float heading, float distance){
-    	Integer[] obstacle = new Integer[4];
-    	int hypotenuse = (int) Math.sqrt((distance*distance+(barrierLength/2)*(barrierLength/2)));
-    	int adjacentSide = (int)Math.cos(heading*hypotenuse);
-    	int oppositeSide = (int)Math.sin(heading*hypotenuse);
-    	
-    	obstacle[0]=posX+oppositeSide;
-    	obstacle[1]=posY+adjacentSide;
-    	obstacle[2]=posX+adjacentSide;
-    	obstacle[3]=posY+oppositeSide;
-    	
-    	obstacles.add(obstacle);
-    }
-    
-    public void newObstaclePoint(float heading, float distance){
-    	Integer[] obstacle = new Integer[2];
-    	
-    	int hypotenuse = (int)distance;
-    	int adjacentSide = (int)Math.cos(heading*hypotenuse);
-    	int oppositeSide = (int)Math.sin(heading*hypotenuse);
-    	
-    	obstacle[0]=posX+oppositeSide;
-    	obstacle[1]=posY+adjacentSide;
-    	
-    	obstaclesPoints.add(obstacle);
-    	
-    }
-    
-	public void paintComponent ( Graphics g )
-	  {
-	    super.paintComponent ( g );
-	    g.translate (width / 2, height / 2);
-	    
-	    //draw grid
-	    drawGrid(g);
-	    
-	    //draw the x and y axes
-	    drawXYAxes (g);
-	    
-	    //draw position
+	public void setXY(int x, int y) {
+		posX = x;
+		posY = y;
+	}
+
+	public void setHeading(float heading) {
+		head = -heading;
+	}
+
+	public void newObstacle(float heading, float distance) {
+		final Integer[] obstacle = new Integer[4];
+		final int hypotenuse = (int)Math.sqrt(distance * distance + barrierLength / 2 * (barrierLength / 2));
+		final int adjacentSide = (int)Math.cos(heading * hypotenuse);
+		final int oppositeSide = (int)Math.sin(heading * hypotenuse);
+
+		obstacle[0] = posX + oppositeSide;
+		obstacle[1] = posY + adjacentSide;
+		obstacle[2] = posX + adjacentSide;
+		obstacle[3] = posY + oppositeSide;
+
+		obstacles.add(obstacle);
+	}
+
+	public void newObstaclePoint(float heading, float distance) {
+		final Integer[] obstacle = new Integer[2];
+
+		final int hypotenuse = (int)distance;
+		final int adjacentSide = (int)Math.cos(heading * hypotenuse);
+		final int oppositeSide = (int)Math.sin(heading * hypotenuse);
+
+		obstacle[0] = posX + oppositeSide;
+		obstacle[1] = posY + adjacentSide;
+
+		obstaclesPoints.add(obstacle);
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.translate(width / 2, height / 2);
+
+		// draw grid
+		drawGrid(g);
+
+		// draw the x and y axes
+		drawXYAxes(g);
+
+		// draw position
 		drawArrowPosition(g);
 
 		/*
-		for(int i=0; i<obstacles.size(); i++){
-			drawObstacleLine(g, obstacles.get(i));
-		}
-		
-		for(int i=0; i<obstacles.size(); i++){
-			drawObstaclePoint(g, obstacles.get(i));
-		}
-		*/
-	  }
-	
+		 * for(int i=0; i<obstacles.size(); i++){
+		 * drawObstacleLine(g, obstacles.get(i));
+		 * }
+		 * for(int i=0; i<obstacles.size(); i++){
+		 * drawObstaclePoint(g, obstacles.get(i));
+		 * }
+		 */
+	}
+
 	private void drawArrowPosition(Graphics g) {
 		g.setColor(new Color(255, 0, 0));
-		int adjacentSide = (int) (Math.cos(Math.toRadians(180 - 45 - head)) * 8);
-		int oppositeSide = (int) (Math.sin(Math.toRadians(180 - 45 - head)) * 8);
-		
+		final int adjacentSide = (int)(Math.cos(Math.toRadians(180 - 45 - head)) * 8);
+		final int oppositeSide = (int)(Math.sin(Math.toRadians(180 - 45 - head)) * 8);
+
 		g.drawLine(posX, -posY, posX - oppositeSide, -posY - adjacentSide);
 		g.drawLine(posX, -posY, posX - adjacentSide, -posY + oppositeSide);
 
-		g.drawLine(posX, -posY, posX - (int) (Math.cos(Math.toRadians(180 - 90 - head)) * 12),
-				-posY + (int) (Math.sin(Math.toRadians(180 - 90 - head)) * 12));
-		
+		g.drawLine(posX, -posY, posX - (int)(Math.cos(Math.toRadians(180 - 90 - head)) * 12),
+				-posY + (int)(Math.sin(Math.toRadians(180 - 90 - head)) * 12));
+
 		/*
-		map.append(new MapData(navi.discrete(posX), navi.discrete(posY), false));
-		  float range = OBSTACLE_DETECTION_RANGE;
-		  float width = OBSTACLE_DETECTION_WIDTH;
-		  while(!(width<0)){
-			  float angleModifier = (float)Math.atan((double)(width/range));
-			  while(!(range<=0)){
-				  System.out.println("draw");
-				  map.append(navi.calcSquare(navi.getNXTData().getPositionX(), navi.getNXTData().getPositionY(), navi.getNXTData().getHeading() + angleModifier, range, false));
-				  map.append(navi.calcSquare(navi.getNXTData().getPositionX(), navi.getNXTData().getPositionY(), navi.getNXTData().getHeading() - angleModifier, range, false));
-				  range -= MAP_SQUARE_LENGTH;
-			  }
-			  range = OBSTACLE_DETECTION_RANGE;
-			  width -= MAP_SQUARE_LENGTH;
-		  }
-		  */
+		 * map.append(new MapData(navi.discrete(posX), navi.discrete(posY), false));
+		 * float range = OBSTACLE_DETECTION_RANGE;
+		 * float width = OBSTACLE_DETECTION_WIDTH;
+		 * while(!(width<0)){
+		 * float angleModifier = (float)Math.atan((double)(width/range));
+		 * while(!(range<=0)){
+		 * System.out.println("draw");
+		 * map.append(navi.calcSquare(navi.getNXTData().getPositionX(), navi.getNXTData().getPositionY(), navi.getNXTData().getHeading() + angleModifier, range, false));
+		 * map.append(navi.calcSquare(navi.getNXTData().getPositionX(), navi.getNXTData().getPositionY(), navi.getNXTData().getHeading() - angleModifier, range, false));
+		 * range -= MAP_SQUARE_LENGTH;
+		 * }
+		 * range = OBSTACLE_DETECTION_RANGE;
+		 * width -= MAP_SQUARE_LENGTH;
+		 * }
+		 */
 	}
 
-	
-	  private void drawXYPoint (Graphics g){
-		  g.setColor(new Color(255,0,0));
-		  g.fillOval(posX-(pointSize/2), -posY-(pointSize/2), pointSize, pointSize);
-		 
-		  map.append(new MapData(navi.discrete(posX), navi.discrete(posY), false));
-		  float range = OBSTACLE_DETECTION_RANGE;
-		  float width = OBSTACLE_DETECTION_WIDTH;
-		  while(!(width<0)){
-			  float angleModifier = (float)Math.atan((double)(width/range));
-			  while(!(range<=0)){
-				  map.append(navi.calcSquare(navi.getNXTData().getPositionX(), navi.getNXTData().getPositionY(), navi.getNXTData().getHeading() + angleModifier, range, false));
-				  map.append(navi.calcSquare(navi.getNXTData().getPositionX(), navi.getNXTData().getPositionY(), navi.getNXTData().getHeading() - angleModifier, range, false));
-				  range -= MAP_SQUARE_LENGTH;
-			  }
-			  range = OBSTACLE_DETECTION_RANGE;
-			  width -= MAP_SQUARE_LENGTH;
-		  }
-	  }
-	  
-	  
-	  /*
-	  private void drawObstaclePoint(Graphics g, Integer[] point){
-		  g.setColor(new Color(0,0,255));
-		  g.fillOval(point[0]-(pointSize/2), -point[1]-(pointSize/2), pointSize, pointSize);
-	  }
-	  
-	  private void drawObstacleLine(Graphics g, Integer[] line){
-		  g.drawLine(line[0], -line[1], line[2], -line[3]);
-	  }
-	*/
-	  private void drawGrid(Graphics g){
-		  MapData help = map;
-		  while(help.next != null){
-			  if(help.getIsObstacle()){
-				  g.setColor(new Color(230,150,121));
-			  }
-			  else{
-				  g.setColor(new Color(184,214,152));
-			  }
-			  g.fillRect(help.getX(), -help.getY(), MAP_SQUARE_LENGTH, MAP_SQUARE_LENGTH);
-			  
-			  help = help.next;
-		  }
-		  help = map;
-		  while(help.previous != null){
-			  if(help.getIsObstacle()){
-				  g.setColor(new Color(230,150,121));
-			  }
-			  else{
-				  g.setColor(new Color(184,214,152));
-			  }
-			  g.fillRect(help.getX(), -help.getY(), MAP_SQUARE_LENGTH, MAP_SQUARE_LENGTH);
-			  
-			  help = help.previous;
-		  }
-	  }
-	  
-	  
-	  private void drawXYAxes (Graphics g) {
-	    //Dimension size = this.getSize();
-	  
-		g.setColor(Color.BLACK);
-		int hBound = width / 2;
-	    int vBound = height / 2;
-	    int tic = width / ticSize;
-				
-	    // draw the x-axis
-	    g.drawLine (-hBound, 0, hBound, 0);
-	    
-	    // draw the tic marks along the x axis
-	    for(int k=0; k<=hBound; k+=ticNumber){
-	    	if((k%100)==0)
-	    	{
-	    	g.drawLine (k, tic*2, k, -tic*2);
-	    	}
-	    	else{
-	    		g.drawLine (k, tic, k, -tic);
-	    	}
-	    }
+	/*
+	 * private void drawObstaclePoint(Graphics g, Integer[] point){
+	 * g.setColor(new Color(0,0,255));
+	 * g.fillOval(point[0]-(pointSize/2), -point[1]-(pointSize/2), pointSize, pointSize);
+	 * }
+	 * private void drawObstacleLine(Graphics g, Integer[] line){
+	 * g.drawLine(line[0], -line[1], line[2], -line[3]);
+	 * }
+	 */
+	private void drawGrid(Graphics g) {
+		for (final Entry<Point, Boolean> e : map.entrySet()) {
+			final Point p = e.getKey();
+			final boolean isObstacle = e.getValue();
 
-	    for(int k=0; k>=-hBound; k-=ticNumber){
-	    	if((k%100)==0)
-	    	{
-	    	g.drawLine (k, tic*2, k, -tic*2);
-	    	}
-	    	else{
-	    		g.drawLine (k, tic, k, -tic);
-	    	}
-	    }
-	    
-	    // draw the y-axis
-	    g.drawLine (0, vBound, 0, -vBound);	
-	    
-	 // draw the tic marks along the y axis
-	    for(int k=0; k<=vBound; k+=ticNumber){
-	    	if((k%100)==0)
-	    	{
-	    	g.drawLine (-tic*2, k, tic*2, k);
-	    	}
-	    	else{
-	    		g.drawLine (-tic, k, tic, k);
-	    	}
-	    }
-	   
-	    for(int k=0; k>=-vBound; k-=ticNumber){
-	    	if((k%100)==0)
-	    	{
-	    	g.drawLine (-tic*2, k, tic*2, k);
-	    	}
-	    	else{
-	    		g.drawLine (-tic, k, tic, k);
-	    	}
-	    }
-	    
-	    g.drawString("x", 265, 15);
+			g.setColor(isObstacle ? new Color(230, 150, 121) : new Color(184, 214, 152));
+			g.fillRect(p.x, -p.y, MAP_SQUARE_LENGTH, MAP_SQUARE_LENGTH);
+		}
+	}
+
+	private void drawXYAxes(Graphics g) {
+		// Dimension size = this.getSize();
+
+		g.setColor(Color.BLACK);
+		final int hBound = width / 2;
+		final int vBound = height / 2;
+		final int tic = width / ticSize;
+
+		// draw the x-axis
+		g.drawLine(-hBound, 0, hBound, 0);
+
+		// draw the tic marks along the x axis
+		for (int k = 0; k <= hBound; k += ticNumber) {
+			if (k % 100 == 0) {
+				g.drawLine(k, tic * 2, k, -tic * 2);
+			} else {
+				g.drawLine(k, tic, k, -tic);
+			}
+		}
+
+		for (int k = 0; k >= -hBound; k -= ticNumber) {
+			if (k % 100 == 0) {
+				g.drawLine(k, tic * 2, k, -tic * 2);
+			} else {
+				g.drawLine(k, tic, k, -tic);
+			}
+		}
+
+		// draw the y-axis
+		g.drawLine(0, vBound, 0, -vBound);
+
+		// draw the tic marks along the y axis
+		for (int k = 0; k <= vBound; k += ticNumber) {
+			if (k % 100 == 0) {
+				g.drawLine(-tic * 2, k, tic * 2, k);
+			} else {
+				g.drawLine(-tic, k, tic, k);
+			}
+		}
+
+		for (int k = 0; k >= -vBound; k -= ticNumber) {
+			if (k % 100 == 0) {
+				g.drawLine(-tic * 2, k, tic * 2, k);
+			} else {
+				g.drawLine(-tic, k, tic, k);
+			}
+		}
+
+		g.drawString("x", 265, 15);
 		g.drawString("y", 7, -125);
 		g.drawString("1m", 91, 15);
-		g.drawString("1m", 7, -95);			
-	  }
-		
+		g.drawString("1m", 7, -95);
+	}
 }
