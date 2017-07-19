5ad0b36007782717bb8a17f2248c7cf03724e000
Justus Breyer <justus.breyer@rwth-aachen.de>
2017-07-19 14:29:41 +0200

    initialized NaviThreads in ApplicationHandler

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/NavigationThread.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/NavigationThread.java
@@ -10,19 +10,25 @@ public class NavigationThread extends Thread{
 	private ApplicationHandler appHandler;
 	private final float xTarget, yTarget;
 	private double xNext, yNext;
+	private boolean running;
 	
-	public NavigationThread(Navigator navi_init, ApplicationHandler app_init, float x_init, float y_init, NXTData data_init){
+	public NavigationThread(Navigator navi_init, ApplicationHandler app_init, float x_init, float y_init){
 		this.navi = navi_init;
 		this.appHandler = app_init;
 		this.xTarget = x_init;
 		this.yTarget = y_init;
 		xNext = Double.MAX_VALUE;
 		yNext = Double.MAX_VALUE;
+		running = true;
+	}
+	
+	public void setRunning(boolean runSet){
+		this.running = runSet;
 	}
 	
 	public void run(){
 		if(xTarget == Float.MAX_VALUE && yTarget == Float.MAX_VALUE){
-			while(true){
+			while(running){
 				appHandler.showBlockedSign(!navi.isFree(navi.getNXTData().getPositionX(), navi.getNXTData().getPositionX()));
 				appHandler.stopMoving();
 				
@@ -34,7 +40,7 @@ public class NavigationThread extends Thread{
 			}
 		}
 		else{
-			while(!navi.reachedPosition(xTarget, yTarget)){
+			while(running && !navi.reachedPosition(xTarget, yTarget)){
 				if((xNext==Double.MAX_VALUE && yNext==Double.MAX_VALUE)|| (navi.reachedPosition((float)xNext, (float)yNext))){
 					Point nextMove = navi.getNextPoint(xTarget, yTarget);
 					xNext = (float) nextMove.getX();
