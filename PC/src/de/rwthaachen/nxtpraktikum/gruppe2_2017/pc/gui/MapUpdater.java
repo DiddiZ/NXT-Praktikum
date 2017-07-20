587d87f4740220467f7a839a5abd71359b4a51d6
Christian Kloos <christian.kloos@rwth-aachen.de>
2017-07-20 14:21:50 +0200

    added static boolean to check if thread is already running

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/MapUpdater.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/MapUpdater.java
@@ -2,10 +2,13 @@ package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui;
 
 public class MapUpdater implements Runnable{
 
+	public static boolean running = false;
 	private UserInterface ui;
 	@Override
 	public void run() {
+		running = true;
 		ui.drawPosition();
+		running = false;
 	}
 	
 	public MapUpdater(UserInterface gui){
