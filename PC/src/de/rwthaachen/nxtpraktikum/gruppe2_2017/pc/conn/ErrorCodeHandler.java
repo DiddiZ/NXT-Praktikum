ce96ea94860463dbbead53eff04ab8695747bd80
Justus <Justus@Justus-PC>
2017-07-24 13:46:32 +0200

    Added JavaDoc for the ErrorCodeHandler
    - deleted unnecessary comments

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/ErrorCodeHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/ErrorCodeHandler.java
@@ -8,21 +8,37 @@ import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui.UserInterface;
 
 /**
- * This class handles the ERROR_CODE messages
- *
  * @author Gregor & Justus
+ * 
+ * This class handles incoming data marked as ERROR_CODES.
+ * Implements the interface CommandHandler and defines the callback-method handle().
  */
+
 public class ErrorCodeHandler implements CommandHandler
 {
 	private final UserInterface ui;
-
+	
+	/**
+	 * The constructor for an ErrorCodeHandler
+	 * @param ui: The UI on which the handler will print its messages and will call methods to handle the error.
+	 */
+	
 	public ErrorCodeHandler(UserInterface ui) {
 		this.ui = ui;
 	}
 
+	
 	@Override
+	/**
+	 * This method reads the incoming ERROR_CODE and switches it.
+	 * Currently recognized ERROR_CODES:
+	 * ERROR_CODE_FALLEN: Display the message, that the NXT has fallen, in the UI and let the UI handle the error.
+	 * ERROR_CODE_PACKAGE_LOSS: Currently just prints a message that this error has been recognized
+	 * Default: Prints a message, that this error is unknown
+	 * 
+	 * @param is: The DataInputStream the handler uses to receive the ERROR_CODE
+	 */
 	public void handle(DataInputStream is) throws IOException {
-		// System.out.println("Error code handler...");
 		final byte param = is.readByte();
 		switch (param) {
 			case ERROR_CODE_FALLEN:
