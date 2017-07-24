5424806a4391b84b9907e830e4d6aa5d3ce5f52e
Justus Breyer <justus.breyer@rwth-aachen.de>
2017-07-24 14:50:11 +0200

    Added JavaDoc in the DisconnectHandler

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/DisconnectHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/DisconnectHandler.java
@@ -6,13 +6,19 @@ import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.comm.CommunicatorNXT;
 
 /**
- * This class handles the COMMAND_DISCONNECT calls.
+ * This class handles incoming data marked as COMMAND_DISCONNECT.
+ * This command is sent by the PC to initialize a disconnect.
+ * Implements the interface CommandHandler and defines the callback-method handle().
  *
  * @author Gregor & Justus
  */
 public class DisconnectHandler implements CommandHandler
 {
 	@Override
+	/**
+	 * This method calls a static method of the CommunicatorNXT to disconnect safely.
+	 * @param is: The DataInputStream the handler could use to receive data. This handler does not use the input-stream. 
+	 */
 	public void handle(DataInputStream is) throws IOException {
 		CommunicatorNXT.staticDisconnect();
 	}
