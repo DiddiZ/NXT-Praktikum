0ae68da73cb470cd8fd92ba0158b03232b27266a
Justus <Justus@Justus-PC>
2017-07-24 14:59:27 +0200

    Added JavaDoc in MoveHandler

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/MoveHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/MoveHandler.java
@@ -6,13 +6,22 @@ import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.nxt.motorcontrol.MotorController;
 
 /**
- * This class handles the COMMAND_MOVE calls.
+ * This class handles data marked as COMMAND_MOVE.
+ * This command is sent by the PC to let the NXT drive a certain distance.
+ * Implements the interface {@link CommandHandler} and defines the callback-method handle().
  *
  * @author Gregor & Justus & Robin
  */
 public class MoveHandler implements CommandHandler
 {
 	@Override
+	/**
+	 * This method reads a float of the input-stream
+	 * and calls a method of the {@link MotorController} to let the NXT move the distance.
+	 * Displays the received command and the distance on the NXT.
+	 * 
+	 * @param is: The DataInputStream the handler uses to receive data.
+	 */
 	public void handle(DataInputStream is) throws IOException {
 		final float distance = is.readFloat();
 		MotorController.move(distance);
