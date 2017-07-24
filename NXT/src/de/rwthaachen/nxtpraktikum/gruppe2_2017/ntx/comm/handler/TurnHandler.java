e734c3570b95f7b004e016ef2cabf185b73a03b9
Justus <Justus@Justus-PC>
2017-07-24 15:03:40 +0200

    Added JavaDoc in TurnHandler

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/TurnHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/TurnHandler.java
@@ -6,16 +6,25 @@ import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.nxt.motorcontrol.MotorController;
 
 /**
- * This class handles the COMMAND_TURN calls.
+ * This class handles data marked as COMMAND_TURN.
+ * This command is sent by the PC to let the NXT turn by a certain angle.
+ * Implements the interface {@link CommandHandler} and defines the callback-method handle().
  *
  * @author Gregor & Justus & Robin
  */
 public class TurnHandler implements CommandHandler
 {
 	@Override
+	/**
+	 * This method reads a float and
+	 * calls a method of the {@link MotorController} to let the NXT turn by the angle.
+	 * Displays the received command and angle on the NXT.
+	 * 
+	 * @param is: The DataInputStream the handler uses to receive data.
+	 */
 	public void handle(DataInputStream is) throws IOException {
 		final float angle = is.readFloat();
 		MotorController.turn(angle);
-		System.out.println("Turning by " + angle + "Â°");
+		System.out.println("Turning by " + angle + "°");
 	}
 }
