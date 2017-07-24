4cbbda7e7a74b6cf48e96b09f00e190a6b4698d4
Justus <Justus@Justus-PC>
2017-07-24 14:27:53 +0200

    Added JavaDoc in LogInfoHandler

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/LogInfoHandler.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/conn/LogInfoHandler.java
@@ -5,13 +5,21 @@ import java.io.IOException;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 
 /**
- * This class handles the ERROR_CODE messages
+ * This class handles incoming data marked as LOG_INFO.
+ * A LOG_INFO is sent by an NXT and contains information that do not fit into regular commands.
+ * Implements the interface CommandHandler and defines the callback method handle.
  *
- * @author Gregor & Justus
+ * @author Justus
  */
 public class LogInfoHandler implements CommandHandler
 {
 	@Override
+	/**
+	 * This method handles the incoming LogInfo-data.
+	 * Reads the length of the incoming LogInfo first and creates an array to save the data.
+	 * Currently only reads the incoming data and prints it as our NXT does not take use of this option.
+	 * To not mess up the DataInputStream the data has to be read in case another NXT takes use of this.
+	 */
 	public void handle(DataInputStream is) throws IOException {
 		final byte length = is.readByte();
 		final byte[] message = new byte[length];
