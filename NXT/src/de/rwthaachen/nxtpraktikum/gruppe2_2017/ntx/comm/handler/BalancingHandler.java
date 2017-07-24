8964139c7ae7b96d5f70a15e6ac7bcfa0137b090
Justus Breyer <justus.breyer@rwth-aachen.de>
2017-07-24 14:45:07 +0200

    Added JavaDoc in BalancingHandler

--- NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/BalancingHandler.java
+++ NXT/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/ntx/comm/handler/BalancingHandler.java
@@ -6,13 +6,22 @@ import de.rwthaachen.nxtpraktikum.gruppe2_2017.comm.CommandHandler;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.ntx.NXT;
 
 /**
- * This class handles the COMMAND_BALANCING calls.
+ * This class handles incoming data marked as COMMAND_BALANCING.
+ * The balancing-command is sent by the PC to either start or stop the balancing of the NXT.
+ * Implements the interface CommandHandler and defines the callback-method handle().
  *
  * @author Gregor & Justus & Robin
  */
 public class BalancingHandler implements CommandHandler
 {
 	@Override
+	/**
+	 * This method reads a boolean of the DataInputStream and
+	 * either starts or stops the balancing of the NXT depending on the value.
+	 * The value is displayed on the NXT.
+	 * 
+	 * @param is: The DataInputStream the handler uses to receive data.
+	 */
 	public void handle(DataInputStream is) throws IOException {
 		final boolean enabled = is.readBoolean();
 		if (enabled)
