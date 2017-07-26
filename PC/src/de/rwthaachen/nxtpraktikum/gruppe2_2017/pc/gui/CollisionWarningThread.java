51551a250ae3832278d67dd92ad14e9e513bbe75
Justus Breyer <justus.breyer@rwth-aachen.de>
2017-07-26 15:19:47 +0200

    Added JavaDoc in CollisionWarningThread

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/CollisionWarningThread.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/CollisionWarningThread.java
@@ -2,12 +2,26 @@ package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui;
 
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.NXTData;
 
+/**
+ * This class is designed to independently detect potential collisions
+ * and show a corresponding warning in the UI.
+ * Implements the {@link Runnable} interface to possibly start it in a separate Thread.
+ * 
+ * @author Fabian
+ */
+
 public class CollisionWarningThread implements Runnable
 {
 	private final UI ui;
 	private final NXTData data;
 	private final Navigator navi;
 
+	/**
+	 * The constructor for a CollisionWarningThread. Assigns attributes that this class uses.
+	 * @param gui: The UI on which a warning will be shown.
+	 * @param data: The NXTData that is needed for calculation.
+	 * @param navi: The Navigator which provides methods to identify nearby obstacles
+	 */
 	public CollisionWarningThread(UI gui, NXTData data, Navigator navi) {
 		ui = gui;
 		this.data = data;
@@ -15,6 +29,14 @@ public class CollisionWarningThread implements Runnable
 	}
 
 	@Override
+	/**
+	 * This method runs as long as the NXT is balancing (the value is taken of the {@link NXTData}).
+	 * It periodically calls a method of the {@link Navigator} to determine whether
+	 * the NXT is currently blocked or not.
+	 * Calls a method of the {@link UI} with this information to show a warning sign
+	 * if needed.
+	 * This method sleeps 100 ms.
+	 */
 	public void run() {
 		while (data.getBalancing()) {
 			ui.showBlockedWay(navi.isBlocked());
