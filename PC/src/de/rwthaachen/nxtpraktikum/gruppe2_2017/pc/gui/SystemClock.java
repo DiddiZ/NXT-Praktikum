01355594155cc4e8308a0e0ce2ce1893298cebf9
Justus Breyer <justus.breyer@rwth-aachen.de>
2017-07-26 16:05:05 +0200

    Added JavaDoc in SystemClock

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/SystemClock.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/gui/SystemClock.java
@@ -3,6 +3,9 @@ package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui;
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.conn.CommunicatorPC;
 
 /**
+ * This class is designed to run a timer as long as the connection endures.
+ * Extends the {@link Thread} class to run it in a separate Thread.
+ * 
  * @author Christian & Robin
  */
 public class SystemClock extends Thread
@@ -10,12 +13,25 @@ public class SystemClock extends Thread
 	private final UserInterface ui;
 	private final CommunicatorPC communicator;
 
+	/**
+	 * The constructor for a SystemClock. Assigns necessary attributes.
+	 * @param ui: The UI which is used to display the connection-time
+	 * @param communicator: The Communicator which is used to determine whether an NXT is connected
+	 */
 	public SystemClock(UserInterface ui, CommunicatorPC communicator) {
 		this.ui = ui;
 		this.communicator = communicator;
 	}
 
 	@Override
+	/**
+	 * This method creates a local variable to save the time the connection was created.
+	 * As long as the connection endures (which is determined by calling a method of the {@link CommunicatorPC}),
+	 * a method of the {@link UserInterface} is called to display
+	 * the difference between the current system time and the local variable.
+	 * 
+	 * The Thread sleeps for 1000 ms.
+	 */
 	public void run() {
 		final long zero = System.currentTimeMillis();
 		while (communicator.isConnected()) {
