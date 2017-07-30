629f413452f47f22cd5efcb8819dbbbcdc835823
Grego Kobsik <gregor.kobsik@rwth-aachen.de>
2017-07-31 00:00:37 +0200

    added Javadoc to EvoAlgorithm

--- PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/PC.java
+++ PC/src/de/rwthaachen/nxtpraktikum/gruppe2_2017/pc/PC.java
@@ -2,6 +2,13 @@ package de.rwthaachen.nxtpraktikum.gruppe2_2017.pc;
 
 import de.rwthaachen.nxtpraktikum.gruppe2_2017.pc.gui.UI;
 
+/**
+ * The main function of the PC Project, that lunches the GUI is started here.
+ * 
+ * @author Christian
+ *
+ */
+
 public class PC
 {
 	/**
